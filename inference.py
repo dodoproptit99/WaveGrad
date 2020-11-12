d5e3cb5d8c1c3d115eeb5f1673b87bdbb36f79e0
dodoproptit99 <dodo.proptit.99@gmail.com>
2020-11-12 13:51:06 +0700

    update

--- inference.py
+++ inference.py
@@ -25,41 +25,41 @@ def get_mel(config, model):
         f_max=config.data_config.f_max,
         n_mels=config.data_config.n_mels,
         window_fn=torch.hann_window
-    )
+    ).cuda()
 
     dataset = AudioDataset(config, training=False)
     test_batch = dataset.sample_test_batch(1)
 
-    n_iter = 25
-    path_to_store_schedule = f'schedules/default/{n_iter}iters.pt'
+    # n_iter = 25
+    # path_to_store_schedule = f'schedules/default/{n_iter}iters.pt'
 
-    iters_best_schedule, stats = iters_schedule_grid_search(
-        model, config,
-        n_iter=n_iter,
-        betas_range=(1e-06, 0.01),
-        test_batch_size=1, step=1,
-        path_to_store_schedule=path_to_store_schedule,
-        save_stats_for_grid=True,
-        verbose=True, n_jobs=4
-    )
+    # iters_best_schedule, stats = iters_schedule_grid_search(
+    #     model, config,
+    #     n_iter=n_iter,
+    #     betas_range=(1e-06, 0.01),
+    #     test_batch_size=1, step=1,
+    #     path_to_store_schedule=path_to_store_schedule,
+    #     save_stats_for_grid=True,
+    #     verbose=True, n_jobs=4
+    # )
     
-    # i=0
-    # for test in tqdm(test_batch):
-    #     mel = mel_fn(test[None])
-    #     start = datetime.now()
-    #     t = time()
-    #     outputs = model.forward(mel, store_intermediate_states=False)
-    #     end = datetime.now()
-    #     print("Time infer: ",str(time()-t))
-    #     outputs = outputs.cpu().squeeze()
-    #     save_path = str(i)+'.wav'
-    #     i+=1
-    #     torchaudio.save(
-    #         save_path, outputs, sample_rate=config.data_config.sample_rate
-    #     )
-    #     inference_time = (end - start).total_seconds()
-    #     rtf = compute_rtf(outputs, inference_time, sample_rate=config.data_config.sample_rate)
-    #     show_message(f'Done. RTF estimate:{np.std(rtf)}')
+    i=0
+    for test in tqdm(test_batch):
+        mel = mel_fn(test[None].cuda())
+        start = datetime.now()
+        t = time()
+        outputs = model.forward(mel, store_intermediate_states=False)
+        end = datetime.now()
+        print("Time infer: ",str(time()-t))
+        outputs = outputs.cpu().squeeze()
+        save_path = str(i)+'.wav'
+        i+=1
+        torchaudio.save(
+            save_path, outputs, sample_rate=config.data_config.sample_rate
+        )
+        inference_time = (end - start).total_seconds()
+        rtf = compute_rtf(outputs, inference_time, sample_rate=config.data_config.sample_rate)
+        show_message(f'Done. RTF estimate:{np.std(rtf)}')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -75,15 +75,18 @@ if __name__ == "__main__":
         '-ns', '--noise_schedule_path', required=True, type=str,
         help='noise schedule, should be just a torch.Tensor array of shape [n_iter]'
     )
-    parser.add_argument(
-        '-m', '--mel_filelist', required=True, type=str,
-        help='mel spectorgram filelist, files of which should be just a torch.Tensor array of shape [n_mels, T]'
-    )
+    # parser.add_argument(
+    #     '-m', '--mel_filelist', required=True, type=str,
+    #     help='mel spectorgram filelist, files of which should be just a torch.Tensor array of shape [n_mels, T]'
+    # )
     parser.add_argument(
         '-v', '--verbose', required=False, type=str2bool,
         nargs='?', const=True, default=True, help='verbosity level'
     )
     args = parser.parse_args()
+
+    # Trying to run inference on GPU
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     # Initialize config
     with open(args.config) as f:
@@ -91,20 +94,17 @@ if __name__ == "__main__":
 
     # Initialize the model
     model = WaveGrad(config)
-    model.load_state_dict(torch.load(args.checkpoint_path,map_location=torch.device('cpu'))['model'], strict=False)
+    model.load_state_dict(torch.load(args.checkpoint_path,map_location=torch.device(device))['model'], strict=False)
     
+    # Set noise schedule
+    noise_schedule = torch.load(args.noise_schedule_path)
+    # print(noise_schedule)
+    n_iter = noise_schedule.shape[-1]
+    init_fn = lambda **kwargs: noise_schedule
+    init_kwargs = {'steps': n_iter}
+    model.set_new_noise_schedule(init_fn, init_kwargs)
 
-    # # Set noise schedule
-    # noise_schedule = torch.load(args.noise_schedule_path)
-    # # print(noise_schedule)
-    # n_iter = noise_schedule.shape[-1]
-    # init_fn = lambda **kwargs: noise_schedule
-    # init_kwargs = {'steps': n_iter}
-    # model.set_new_noise_schedule(init_fn, init_kwargs)
-
-    # Trying to run inference on GPU
-    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    device = 'cpu'
+    # device = 'cpu'
     model = model.to(device)
     get_mel(config, model)
 
